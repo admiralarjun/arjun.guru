7a089851855390f1da366eafb4f9b700aac6fe67
ARJUN S <arjunevershine@gmail.com>
2025-01-27 23:19:55 +0530

    Update

--- index.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('a').forEach(link => {
-    link.setAttribute('target', '_blank'); // Open in a new tab
-    link.setAttribute('rel', 'noopener noreferrer'); // Improve security and performance
-});
